52b62cf47be5b0320093a4a232c3a6702e937ca4
joaohhenriq <joao.camargo@avenuesec.io>
2021-09-27 12:51:42 -0300

    Connectivity check implementation

--- lib/core/platform/network_info.dart
+++ lib/core/platform/network_info.dart
@@ -1,3 +1,21 @@
+import 'package:connectivity/connectivity.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+
+class NetworkImpl implements NetworkInfo {
+  final Connectivity connectivity;
+
+  NetworkImpl(this.connectivity);
+
+  @override
+  Future<bool> get isConnected async {
+    var mobileConnected = await connectivity.checkConnectivity();
+    if (mobileConnected == ConnectivityResult.mobile || mobileConnected == ConnectivityResult.wifi) {
+      return true;
+    }
+
+    return false;
+  }
 }
